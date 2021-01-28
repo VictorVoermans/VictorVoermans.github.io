d7c826de5e5b79538184d73781432315d808793c
VictorVoermans <s2963779@vuw.leidenuniv.nl>
2021-01-28 16:56:54 +0100

    no prefetch anymore

--- prefetch.js
+++
@@ -1,8 +0,0 @@
-function prefetch() {
-	for (i = 0; i < images.length; i++) {
-		var image = document.createElement("img");
-		image.src = images[i];
-		image.style.width = "1px";
-		document.getElementById("prefetch").appendChild(image);
-	}
-}
